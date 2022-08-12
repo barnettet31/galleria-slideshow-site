3d10a0e276aba34ad731c58a4d28a3438d9f540a
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-08-12 11:04:12 -0400

    created dynamic routes for react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import './App.css';
-import {data} from  './data/data.js'
-import { Home } from './routes/home/home.component';
-import { Artwork } from './routes/artwork/artwork.component';
-import {Routes, Route} from 'react-router-dom';
-import { Header } from './components/header/header.component';
-import { constructRoutes } from './utilities/string.utilities';
+import "./App.css";
+import { data } from "./data/data.js";
+import { Home } from "./routes/home/home.component";
+import { Artwork } from "./routes/artwork/artwork.component";
+import { Routes, Route } from "react-router-dom";
+import { Header } from "./components/header/header.component";
+import { constructRoutes } from "./utilities/string.utilities";
 function App() {
-
   return (
     <Routes>
-    <Route element={<Header/>}>
-      <Route path='/' element={<Home artworks={data}/>} />
-      {data.map((art, index)=>
-        <Route key={art.name+Math.random()} path={constructRoutes(art.name)} element={<Artwork art={art} nextItem={data[index+1]}/>}/>
-      )}</Route>
+      <Route element={<Header />}>
+        <Route path="/" element={<Home artworks={data} />} />
+        {data.map((art, index) => (
+          <Route
+            key={art.name + Math.random()}
+            path={constructRoutes(art.name)}
+            element={<Artwork art={art} nextItem={data[index + 1]} />}
+          />
+        ))}
+      </Route>
     </Routes>
-);
+  );
 }
 
 export default App;
