9b3518834311b274d056db91e8ed0f6f872a7c9d
barnettet31 <74745161+barnettet31@users.noreply.github.com>
2022-08-12 10:59:15 -0400

    changed tailwind config for custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        sm: ["18px", "22px"],
+        lg: ["24px", "29px"],
+        xl: ["56px", "64px"],
+      },
+      colors: {
+        "dark-grey": "#7D7D7D",
+        "med-grey": "#e5e5e5",
+        "light-grey": "#f3f3f3",
+      },
+    },
   },
   plugins: [],
-}
+};
